466b8a511cfdbd4971fcc17f282ca2e244d0c023
digitaldisarray <electrichexagon8@gmail.com>
2018-01-26 17:21:01 -0800

    Added download link creator.

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,12 +13,12 @@ public class Program {
 	private static String temp; // String for storing temporary information.
 
 	// TODO: Fix. Everything. (messy code)
-	
-	/* Prepare to cringe at this messy code. Sorry in advance.
-	 * Prepare to cringe at this messy code. Sorry in advance.
-	 * Prepare to cringe at this messy code. Sorry in advance.
-	 * Prepare to cringe at this messy code. Sorry in advance.
-	 */ 
+
+	/*
+	 * Prepare to cringe at this messy code. Sorry in advance. Prepare to cringe at
+	 * this messy code. Sorry in advance. Prepare to cringe at this messy code.
+	 * Sorry in advance. Prepare to cringe at this messy code. Sorry in advance.
+	 */
 
 	public static void input(String input) {
 		// Check if input is valid
@@ -41,6 +42,9 @@ public class Program {
 				downloadReadme();
 				break;
 			case 5:
+				txtDownloadLinks();
+				break;
+			case 6:
 				exit();
 				break;
 			}
@@ -48,18 +52,77 @@ public class Program {
 
 	}
 
+	// Generate repo download links from a txt
+	private static void txtDownloadLinks() {
+		temp = getInput("File with links: ");
+		if (!temp.endsWith(".txt")) {
+			temp += ".txt";
+		}
+
+		temp = readLinks(temp);
+		String[] links = temp.split("@@&&??");
+		
+		temp = getInput("Output file: ");
+		if (!temp.endsWith(".txt")) {
+			temp += ".txt";
+		}
+
+		try {
+			File file = new File(temp);
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			String[] linksConverted = links;
+			
+			for (int i = 0; i < links.length - 1; i++) {
+				System.out.println(i);
+				
+				linksConverted[i] = linkToDownload(links[i]);
+				
+				fw.write(linksConverted[i] + "\n");
+			}
+			fw.close();
+		} catch (IOException e) {
+			// Ignored.
+		}
+
+	}
+
+	// Convert a link to a download link.
+	private static String linkToDownload(String link) {
+		String[] raw = link.split("/");
+		return "https://github.com/" + raw[3] + "/" + raw[4] + "/archive/master.zip";
+	}
+
+	// Read links from a file
+	private static String readLinks(String name) {
+		try {
+			// Create BufferedReader, StringBuilder, and String line.
+			BufferedReader br = new BufferedReader(new FileReader(name));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			// Read the file while there is still content.
+			while (line != null) {
+				sb.append(line + "@@&&??");
+				line = br.readLine();
+			}
+			temp = sb.toString();
+			br.close();
+		} catch (IOException e) {
+			System.err.println("IOException while reading file.");
+			// e.printStackTrace();
+		}
+		return temp;
+	}
+
 	// Download a readme file
 	private static void downloadReadme() {
 		temp = getInput("Enter url: ");
-		if(temp.startsWith("https://github.com/")) {
+		if (temp.startsWith("https://github.com/")) {
 			temp = repoToRead(temp);
 		}
 
-		URL url;
-		
-		
 		try {
-			url = new URL(temp);
+			URL url = new URL(temp);
 			URLConnection conn = url.openConnection();
 			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String inputLine;
@@ -68,7 +131,7 @@ public class Program {
 			if (!temp.endsWith(".txt")) {
 				temp += ".txt";
 			}
-			
+
 			File file = new File(temp);
 
 			if (!file.exists()) {
@@ -83,7 +146,7 @@ public class Program {
 			while ((inputLine = br.readLine()) != null) {
 				bw.write(inputLine + "\n");
 			}
-			
+
 			bw.close();
 			br.close();
 
@@ -112,9 +175,9 @@ public class Program {
 
 	// Display help information.
 	private static void help() {
-		System.out.println("Command 1: Display help.\n" + "Command 2: Convert a readme.md link to a repo link.\n"
-				+ "Command 3: Convert a repo link to a readme link.\n"
-				+ "Command 4: Download a readme from a repo or readme url.");
+		System.out.println("1: Display help.\n" + "2: Convert a readme.md link to a repo link.\n"
+				+ "3: Convert a repo link to a readme link.\n" + "4: Download a readme from a repo or readme url.\n"
+				+ "5: Generate download links from a txt of repos.");
 	}
 
 	// Prompt user to exit the program.
--- src/Strings.java
+++ src/Strings.java
@@ -1,12 +1,13 @@
 
 public class Strings {
 	public static String title = "===== Welcome to GitReader =====";
-	public static String menu = "Git Reader v0.1\n"
+	public static String menu = "Git Reader v0.1.1\n"
 			+ "1) Info/Help\n"
 			+ "2) Readme to repo.\n"
 			+ "3) Repo to readme.\n"
 			+ "4) Download readme from link.\n"
-			+ "5) Exit";
+			+ "5) Repo list to download links.\n"
+			+ "6) Exit";
 	public static String prompt = ">";
 	public static String error =  "[ ERROR ] - ";
 	public static String warn =   "[ WARN ] - ";
