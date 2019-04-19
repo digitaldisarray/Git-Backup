a9a2d4250d06d7cddce21f21c27703b9854f673e
Kevin Rossel <electrichexagon8@gmail.com>
2019-04-18 19:07:38 -0700

    Better code

--- python version/gitbackup.py
+++ python version/gitbackup.py
@@ -5,18 +5,11 @@ from datetime import datetime
 with open('repos.txt') as f:
     lines = f.readlines()
 
-t = ''
-
 # Only add the lines with links in them
-# https://github.com/digitaldisarray/Git-Backup
-# https://github.com/digitaldisarray/Git-Backup/archive/master.zip
 for line in lines:
 	if line.startswith('http'):
 		r = requests.get(line.strip())
-		t = datetime.now().strftime('%Y-%m-%d %Hh%Mm%Ss')
-		print('Saving to: ./' + line.split('/')[4] + '-master.zip')
+		t = datetime.now().strftime('%Y-%m-%d %Hh %Mm %Ss') # We get the time each time in the loop rather than once in case the script takes a while
+		print('Saving: ' + line.split('/')[4] + '-master' + t + '.zip')
 		with open('./' + line.split('/')[4] + '-master ' + t + '.zip', 'wb') as f:
 			f.write(r.content)
-		print(r.status_code)  
-		print(r.headers['content-type'])  
-		print(r.encoding)  
