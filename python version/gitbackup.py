5ad0e780112415aa35bb90cf6d5cccdd91979652
Kevin Rossel <electrichexagon8@gmail.com>
2019-04-19 15:32:59 -0700

    Added link conversion

--- python version/gitbackup.py
+++ python version/gitbackup.py
@@ -2,10 +2,19 @@ import requests
 from datetime import datetime
 
 # Todo:
-# Link conversion
 # Entire profile backups
+# Entire organization backup
 # Config file
-# Sorting options
+# Sorting options in config file
+# Able to select different branches in config file
+
+# Converts https://github.com/digitaldisarray/Git-Backup
+# To: https://github.com/digitaldisarray/Git-Backup/archive/master.zip
+def repo_to_dl(repolink):
+	if repolink.endswith('/'):
+		return repolink + "archive/master.zip"
+	else:
+		return repolink + "/archive/master.zip"
 
 with open('repos.txt') as f:
     lines = f.readlines()
@@ -13,8 +22,12 @@ with open('repos.txt') as f:
 # Only add the lines with links in them
 for line in lines:
 	if line.startswith('http'):
+		if line.endswith('.zip') == False:
+			line = repo_to_dl(line)
+			
 		r = requests.get(line.strip())
 		t = datetime.now().strftime('%Y-%m-%d %Hh %Mm %Ss') # We get the time each time in the loop rather than once in case the script takes a while
+		
 		print('Saving: ' + line.split('/')[4] + '-master' + t + '.zip')
 		with open('./' + line.split('/')[4] + '-master ' + t + '.zip', 'wb') as f:
 			f.write(r.content)
